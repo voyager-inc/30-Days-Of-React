a17a7132e5f1fafd32fa63eeacfdaddaaba7d026
vuongducdai <daivuong7696@gmail.com>
2022-08-04 15:56:03 +0700

    update day 18

--- solutions/day-18/fetchaxios/src/index.js
+++ solutions/day-18/fetchaxios/src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Country = ({
+  country: { name, capital, flag, languages, population, currency },
+}) => {
+  return (
+    <div className='country'>
+      <div className='country_flag'>
+        <img src={flag} alt={name} />
+      </div>
+      <h3 className='country_name'>{name.toUpperCase()}</h3>
+      <div class='country_text'>
+        <p>
+          <span>Population: </span>
+          {population}
+        </p>
+      </div>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    const url = 'https://restcountries.eu/rest/v2/all'
+    axios
+      .get(url)
+      .then((response) => {
+        this.setState({
+          data: response.data,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>React Component Life Cycle</h1>
+        <h1>Calling API</h1>
+        <div>
+          <p>There are {this.state.data.length} countries in the api</p>
+          <div className='countries-wrapper'>
+            {this.state.data.map((country) => (
+              <Country country={country} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
